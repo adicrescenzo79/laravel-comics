7c68ecb9df20fb55450323858531014905022fd8
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-12 19:26:05 +0200

    ciclo riuscito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,17 @@
   <div class="container">
     <h2 class="titolo txt-uppercase">current series</h2>
 
-    @foreach ($volumi as $volume)
-      <p>{{$volume->title}}</p>
-    @endforeach
+    <div class="row">
+      @foreach ($volumi as $volume)
+        <div class="col-lg-2">
+          <div class="content">
+            <img src="{{$volume['thumb']}}" alt="">
+            <p class="txt-uppercase">{{$volume['series']}}</p>
+          </div>
+        </div>
+      @endforeach
+    </div>
+
 
   </div>
 
