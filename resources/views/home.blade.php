4069f91b4ced53cd86b49d6ba903d6099067cde2
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-12 19:59:06 +0200

    visualizzate tutte le card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,21 @@
     <h2 class="titolo txt-uppercase">current series</h2>
 
     <div class="row">
-      @foreach ($volumi as $volume)
+      @foreach ($volumi as $key => $volume)
         <div class="col-lg-2">
-          <div class="content">
-            <img src="{{$volume['thumb']}}" alt="">
-            <p class="txt-uppercase">{{$volume['series']}}</p>
-          </div>
+          <a href="/">
+            <div class="card">
+              <div class="cover" style="background-image:url('{{$volume['thumb']}}');">
+              </div>
+              <p class="txt-uppercase mt-1">{{$volume['series']}}</p>
+            </div>
+          </a>
         </div>
       @endforeach
     </div>
 
 
   </div>
-
-
-
-
 
 
 
