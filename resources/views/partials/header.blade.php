1fe361cd323189deabdc30c049174f50874f918f
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-12 16:23:16 +0200

    header nav up completo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,22 @@
 <header>
-  <b>CHARACTERS</b>
+  <nav>
+
+    <div class="up">
+      <div class="container flex">
+        <ul class="flex">
+          <li class="txt-uppercase"><a href="#">dc power&#8480; visa&#174;</a></li>
+          <li class="txt-uppercase">
+          <a href="#">additional dc series ▼</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="menu">
+      <div class="container">
+        <img class="logo" src="/img/dc-logo.png" alt="logo">
+      </div>
+    </div>
+
+  </nav>
+
 </header>
